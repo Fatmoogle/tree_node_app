6b2e8c19aecc4b24b389a85e80f232e80da158be
Alex Varela <alexvar93@gmail.com>
2021-12-02 15:00:30 -0500

    added tree menu library

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import TreeMenu from 'react-simple-tree-menu';
+import '../node_modules/react-simple-tree-menu/dist/main.css';
 
 function App() {
+  
+  const treeData = [
+    {
+      key: 'Main node',
+      label: 'Main node',
+      nodes: [
+        {
+          key: 'second-level-node-1',
+          label: 'Node 1 at the second level',
+          nodes: []
+        },
+      ],
+    }
+  ];
+  
   return (
     <div className="App">
       <h1>Hello!</h1>
+      <TreeMenu data={treeData} hasSearch={false}></TreeMenu>
     </div>
   );
 }
